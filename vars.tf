49460bda3d5465175ca1a15ccfeb178b42ab8024
Natan Lao <natanlao@users.noreply.github.com>
2022-01-25 00:05:05 -0800

    Add validation to var.forward_to

--- vars.tf
+++ vars.tf
@@ -1,8 +1,16 @@
 variable "area_code" {
-  type    = string
+  type     = string
+  nullable = false
 }
 
 variable "forward_to" {
-  type    = string
+  type        = string
+  description = "Phone number to forward SMS and voice to."
+  nullable    = false
+
+  validation {
+    condition     = can(regex("^\\+?[1-9]\\d{1,14}$", var.forward_to))
+    error_message = "Phone number must comply with E.164."
+  }
 }
 
