63c22ed622edc1a0245c6cecc03fd4ec7f87cb27
Natan Lao <natanlao@users.noreply.github.com>
2022-11-06 10:26:54 -0800

    Expose function outputs

--- outputs.tf
+++ outputs.tf
@@ -1,4 +1,9 @@
-output "phone_number" {
-  value = twilio_phone_number.phone_number.phone_number
+output "message_forwarding_function_url" {
+  value       = "https://${twilio_serverless_environment.forwarding.domain_name}${twilio_serverless_function.sms.path}"
+  description = "URL of the function that forwards messages. Configure your phone number to POST to this URL for messages."
 }
 
+output "voice_forwarding_function_url" {
+  value       = "https://${twilio_serverless_environment.forwarding.domain_name}${twilio_serverless_function.voice.path}"
+  description = "URL of the function that forwards voice. Configure your phone number to POST to this URL for voice."
+}
